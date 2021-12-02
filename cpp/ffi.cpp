78b2fa503d6f80eae612250b6f993fc4c7cecd30
Arthur Paulino <arthurleonardo.ap@gmail.com>
2021-12-01 23:36:45 -0300

    copyright on cpp file

--- cpp/ffi.cpp
+++ cpp/ffi.cpp
@@ -1,8 +1,13 @@
+/*
+    Copyright (c) 2021 Arthur Paulino. All rights reserved.
+    Released under Apache 2.0 license as described in the file LICENSE.
+    Authors: Arthur Paulino
+*/
+
 #include <lean/lean.h>
 #include <mysql/mysql.h>
 #include <mysql/field_types.h>
 #include <string.h>
-#include <stdio.h>
 
 #define internal inline static
 #define external extern "C"
